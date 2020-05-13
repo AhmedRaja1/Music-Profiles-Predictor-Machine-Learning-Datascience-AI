3d157391d76fc55cd4360604c1536a0432f5658f
RAJA AHMED <ahmedrajay@gmail.com>
2020-05-14 01:14:03 +0500

    Add files via upload

---
+++ ML project on music profiles.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+music_data = pd.read_csv('music.csv')
+x = music_data.drop(columns=['genre'])
+y = music_data['genre']
+
+x_test, x_train, y_test, y_train = train_test_split(x, y, test_size=0.2)
+
+model = DecisionTreeClassifier()
+model.fit(x_train, y_train)
+predictions = model.predict(x_test)
+
+score = accuracy_score(y_test, predictions)
+score
+
